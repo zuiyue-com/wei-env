3f90cfd1bd72d9ada3deb7062dd25639026ab98d
HuangWei <market@zuiyue.com>
2023-08-10 16:53:53 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn home_dir() -> Result<String, Box<dyn std::error::Error>> {
+    let home_dir = if cfg!(target_os = "windows") {
+        std::env::var("USERPROFILE")?
+    } else {
+        std::env::var("HOME")?
+    };
+    Ok(home_dir)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn uuid_dir() -> String {
+    let home_dir = home_dir().unwrap();
+    let file_path;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    if cfg!(target_os = "windows") {
+        file_path = format!("{}/AppData/Local/Ai/uuid.dat", home_dir);
+    } else {
+        file_path = format!("{}/uuid.dat", home_dir);
     }
+    file_path
+}
+
+pub fn user_dir() -> String {
+    let home_dir = home_dir().unwrap();
+    let file_path;
+
+    if cfg!(target_os = "windows") {
+        file_path = format!("{}/AppData/Local/Ai/user.dat", home_dir);
+    } else {
+        file_path = format!("{}/user.dat", home_dir);
+    }
+    file_path
 }
